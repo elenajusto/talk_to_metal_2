dcb9bfe577b5af116794349bfdfe79e0c0a90cb4
Elena Justo <intern@arlula.com>
2025-11-03 20:12:17 +1100

    code to look at pointers

--- main.c
+++ main.c
@@ -1,27 +1,11 @@
-int global_variable = 10;         // Should start off as 10
-int uninitialised_global_variable;  // Should start off as zero
+int counter = 0;
 
 int main(void) { 
-    int local_variable = 20;                        // Should be 20 
-    
-    static int static_local_variable = 30;          // Should be 30
+    int *pointer_integer;               // pointer_integer is a pointer (variable to a memory address) to an integer variable
+    pointer_integer = &counter;         // pointer_integer is now tracking the memory address for the counter variable
 
-    static int uninitialised_static_local_variable; // Should be zero
-
-    static int sum;                                 // Should be zero
-    
-    sum =   global_variable + uninitialised_global_variable +
-            local_variable + static_local_variable +
-            uninitialised_static_local_variable;
-    
-    if (sum==60) {
-
-        // If everything is working fine then we should loop here
-        while(1) {}
-
-    } else {
-
-        // We should not reach this condition
-        return 0;
+    while (*pointer_integer < 21) {     // de-referencing allows us to susbtitue the value stored at the pointer/address 
+        (*pointer_integer)++;
     }
+    return 0;
 }
