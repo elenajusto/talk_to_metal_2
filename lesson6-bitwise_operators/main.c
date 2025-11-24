eb326becf3b5b53422989004c06f617b5e1e1a57
Elena Justo <intern@arlula.com>
2025-11-24 16:51:34 +1100

    use bitwise operators

--- lesson6-bitwise_operators/main.c
+++ lesson6-bitwise_operators/main.c
@@ -4,19 +4,34 @@
 #define GPIOA_MODER (int*)(GPIOA_BASE + 0x0)
 #define GPIOA_ODR (int*)(GPIOA_BASE + 0x14)
 
+#define GPIOA_PIN10 (1<<10)
+#define GPIOA_PIN2 (1<<2)
+#define GPIOA_PIN4 (1<<4)
+#define GPIOA_PIN6 (1<<6)
+
 int main(void) { 
     *RCC_IOPENR = 0x1;
     *GPIOA_MODER = 0xEBDFFFFF;
 
+    // Debug examples to be seen in memory viewer
+    *GPIOA_ODR |= GPIOA_PIN2;
+    *GPIOA_ODR |= GPIOA_PIN4;
+    *GPIOA_ODR |= GPIOA_PIN6;
+
+    *GPIOA_ODR &= ~(GPIOA_PIN2);
+    *GPIOA_ODR &= ~(GPIOA_PIN4);
+    *GPIOA_ODR &= ~(GPIOA_PIN6);
+    
+    // Blink LED
     while(1){
-        *GPIOA_ODR = 0x400;
+        *GPIOA_ODR |= GPIOA_PIN10;
 
         volatile int counter = 0;
         while (counter < 1000000) {
             counter++;
         }
         
-        *GPIOA_ODR = 0x0;
+        *GPIOA_ODR &= ~(GPIOA_PIN10);
 
         counter = 0;
         while (counter < 1000000) {
