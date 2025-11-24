21498e7aef1bfc3fe355b15064d76598cf4d7598
Elena Justo <intern@arlula.com>
2025-11-24 12:30:32 +1100

    start lesson4 based on stm datasheet

--- lesson4-control_outside/main.c
+++ lesson4-control_outside/main.c
@@ -1,14 +1,46 @@
 int main(void) { 
-    int *pointer_integer_1;               // a pointer (variable to a memory address) to an integer variable
-    *pointer_integer_1 = 0xDEADBEEF;      // assign the memory located at this pointer to the value 0xDEADBEEF
+    
+    // GPIO port mode register (GPIOx_MODER)
+    /* 
+    MODEy[1:0]: Port x configuration I/O pin y (y = 15 to 0) 
+    These bits are written by software to configure the I/O mode. 
+    00: Input mode 
+    01: General purpose output mode 
+    10: Alternate function mode 
+    11: Analog mode (reset state)
+    */
 
-    int *pointer_integer_2;
-    pointer_integer_2 = (int*)0x20000040;   // directly assign this pointer to a specific flash memory adddress
-    *pointer_integer_2 = 0xCAFED00D;        // assign this piece of memory the value 0xCAFED00D
+    // Set Port A Pin 10 to output mode
 
-    int *pointer_integer_3;
-    pointer_integer_3 = (int*)pointer_integer_2+1;    // assign this pointer near pinter 2
-    *pointer_integer_3 = 0xCAFEBABE;                // assign it 0xCAFEBABE
+    /*
+    Port A starts at 0x5000 0000
+    GPIOx_MODER has offset 0x00 from the start of port
+    Pin 10 which is MODE10[1:0] is at Bit 20 and Bit 21
+    We want Bit 20 and Bit 21 to be 01 (0x1)
+    Bit 20 and Bit 21 default value is 11 (0xB)
+    */
+    int *pointer_moder;
+    pointer_moder = (int*)0x50000000;             // directly assign this pointer to a the MODER register for GPIOA
+    *pointer_moder = 0xEBDFFFFF;                  // assign it 0x1
+    
+
+    // While(1)
+        // GPIO port output data register (GPIOx_ODR) - HIGH
+        /*
+        OD[15:0]: Port output data I/O pin y (y = 15 to 0) 
+        These bits can be read and written by software.  
+        Note: For atomic bit set/reset, the OD bits 
+        can be individually set and/or reset by writing 
+        to the GPIOx_BSRR register (x = A to D, F).
+        */
+
+        // Set Port A Pin 10 to 1
+
+        // Delay
+
+        // GPIO port output data register (GPIOx_ODR) - LOW
+
+        // Set Port A Pin 10 to 0
 
     return 0;
 }
