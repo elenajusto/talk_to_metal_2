13439a7ce549f4d939c49180a8f80da41ba9a909
Elena Justo <intern@arlula.com>
2025-11-29 13:40:03 +1100

    blink port B led's

--- lesson6-bitwise_operators/main.c
+++ lesson6-bitwise_operators/main.c
@@ -1,30 +1,28 @@
 #include "cmsis/stm32g071xx.h"
 
 #define RCC_IOPENR (int*)(RCC_BASE + 0x34)
+
+// GPIO A
 #define GPIOA_MODER (int*)(GPIOA_BASE + 0x0)
 #define GPIOA_ODR (int*)(GPIOA_BASE + 0x14)
+#define GPIOA_PIN10 (1<<10)                     // Pin D2 PA10
 
-#define GPIOA_PIN10 (1<<10)
-#define GPIOA_PIN2 (1<<2)
-#define GPIOA_PIN4 (1<<4)
-#define GPIOA_PIN6 (1<<6)
+// GPIO B
+#define GPIOB_MODER (int*)(GPIOB_BASE + 0x0)    // 0x50000400 + 0x0
+#define GPIOB_ODR (int*)(GPIOB_BASE + 0x14)     // 0x50000400 + 0x14 = 0x50000414
+#define GPIOB_PIN3 (1<<3)                       // Pin D3 PB3
+#define GPIOB_PIN10 (1<<10)                     // Pin D4 PB10
 
 int main(void) { 
-    *RCC_IOPENR = 0x1;
-    *GPIOA_MODER = 0xEBDFFFFF;
-
-    // Debug examples to be seen in memory viewer
-    *GPIOA_ODR |= GPIOA_PIN2;
-    *GPIOA_ODR |= GPIOA_PIN4;
-    *GPIOA_ODR |= GPIOA_PIN6;
-
-    *GPIOA_ODR &= ~(GPIOA_PIN2);
-    *GPIOA_ODR &= ~(GPIOA_PIN4);
-    *GPIOA_ODR &= ~(GPIOA_PIN6);
+    *RCC_IOPENR = 0x3;                          // Set bit 0 and bit 1 to 1 (GPIOAEN and GPIObEN respectively)
+    *GPIOA_MODER = 0xEBDFFFFF;                  // Set bit 21 to 0
+    *GPIOB_MODER = 0xFFDFFF7F;                  // Set bit 7 and bit 21 to 0
     
     // Blink LED
     while(1){
         *GPIOA_ODR |= GPIOA_PIN10;
+        *GPIOB_ODR |= GPIOB_PIN3;
+        *GPIOB_ODR |= GPIOB_PIN10;
 
         volatile int counter = 0;
         while (counter < 1000000) {
@@ -32,6 +30,8 @@ int main(void) {
         }
         
         *GPIOA_ODR &= ~(GPIOA_PIN10);
+        *GPIOB_ODR &= ~(GPIOB_PIN3);
+        *GPIOB_ODR &= ~(GPIOB_PIN10);
 
         counter = 0;
         while (counter < 1000000) {
